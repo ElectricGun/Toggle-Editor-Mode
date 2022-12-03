38d7136059de4da478c7c1ad0d13f5f81986e04b
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2022-12-04 00:40:43 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,18 @@ Events.on(ClientLoadEvent, () => {
 
     let tablew = 50;
     let tableh = 50;
+    let padTop = 6;
+    let padLeft = 6;
 
-    var table = new Table().bottom().left();
+    var table = 0;
+    if (Vars.mobile == true) {
+        table = new Table().bottom().left();
+    } else  {
+        table = new Table().top().left();
+    };
+
     let button = TextButton("Off")
-    table.add(button).size(tablew, tableh).padLeft(6).padTop(-6);
+    table.add(button).size(tablew, tableh).padLeft(padLeft).padTop(padTop);
     table.y = (tablew, tableh)
     
     Events.run(Trigger.update, () => {
@@ -28,8 +36,6 @@ Events.on(ClientLoadEvent, () => {
     
         Vars.state.rules.editor = isEditor;
             
-
-//        Log.info("Editor is" + isEditor);
     });
     
 
